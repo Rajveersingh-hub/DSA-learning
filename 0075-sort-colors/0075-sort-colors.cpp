790bc4ab722a945db7700a43910a32edadb89596
Rajveer Singh <Rajveersingh173234@gmail.com>
2026-07-02 08:53:05 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int ct0=0;
+        int ct1=0;
+        int ct2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                ct0++;
+            }
+            else if(nums[i]==1){
+                ct1++;
+            }
+            else{
+                ct2++;
+            }
+
+        }
+        for(int i=0;i<ct0;i++){
+            nums[i]=0;
+        }
+        for(int i=ct0;i<ct0+ct1;i++){
+            nums[i]=1;
+        }
+        for(int i=ct0+ct1;i<nums.size();i++){
+            nums[i]=2;
+        }
+
+        
+    }
+};
