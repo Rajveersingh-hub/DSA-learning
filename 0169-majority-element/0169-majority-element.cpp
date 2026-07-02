18736a2b1aee930f62f111834ce216d7e74a6c45
Rajveer Singh <Rajveersingh173234@gmail.com>
2026-07-02 09:16:47 +0530

    Time: 0 ms (100%), Space: 28.2 MB (66.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // int n=nums.size();
-        // int frq=0;
-        // int ans=0;
-        // for(int i=0;i<n;i++){
-        //     if(frq==0){
-        //         ans=nums[i];
-        //     }
-        //     if(ans==nums[i]){
-        //         frq++;
-        //     }
-        //     frq--;
-        // }
-        // return ans;
-        unordered_map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second>(nums.size())/2){
-                return it.first;
+        int n=nums.size();
+        int frq=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(frq==0){
+                frq=1;
+                ans=nums[i];
             }
+            else if(ans==nums[i]){
+                frq++;
+            }
+            else{
+            frq--;}
         }
-        return -1;
+        return ans;
+        // unordered_map<int,int>mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it:mpp){
+        //     if(it.second>(nums.size())/2){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
     }
 };
