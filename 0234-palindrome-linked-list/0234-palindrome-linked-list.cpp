346f74b04c8a7ea300d380902aee47e39140f9aa
Rajveer Singh <Rajveersingh173234@gmail.com>
2026-08-08 11:47:11 +0530

    Time: 6 ms (29.99%), Space: 122 MB (39.58%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,25 +9,79 @@
  * };
  */
 class Solution {
+    private:
+    ListNode* middle(ListNode* head){
+       ListNode*slow=head;
+       ListNode*fast=head->next;
+       while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next;
+        if(fast->next!=NULL){
+            fast=fast->next;
+        }
+        
+       }
+       return slow;
+    }
+    ListNode* reverse(ListNode*head){
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int>data;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            data.push_back(temp->val);
-            temp=temp->next;
-        }
-        int st=0;
-        int end=data.size()-1;
-        while(st<=end){
-            if(data[st]==data[end]){
-                st++;
-                end--;
-            }
-            else{
-                return false;
-            }
-        }
+        //Aproch-1 TC-O(N) SC-O(N)
+        // vector<int>data;
+        // ListNode*temp=head;
+        // while(temp!=NULL){
+        //     data.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int st=0;
+        // int end=data.size()-1;
+        // while(st<=end){
+        //     if(data[st]==data[end]){
+        //         st++;
+        //         end--;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+      //Aproch-2 TC-O(N) SC-O(N)
+      if(head->next==NULL){
         return true;
+      }
+    //step-1 get middle
+    ListNode*middlenode=middle(head);
+    //step-2 revrse the ll after middle
+    ListNode*temp=middlenode->next;
+    middlenode->next=reverse(temp);
+    // step-3 compare both data reverse and middle
+    ListNode*head1=head;
+    ListNode*head2=middlenode->next;
+    while(head2!=NULL){
+        if(head1->val!=head2->val){
+            return false;
+        }
+        else{
+            head1=head1->next;
+            head2=head2->next;
+        }
+    }
+     temp=middlenode->next;
+    middlenode->next=reverse(temp);
+
+    return true;
+
     }
 };
